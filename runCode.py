ef34d678841d325dc0ff33320baaacc72157892d
David Cuellar <david.cuellar@javeriana.edu.co>
2022-02-12 18:51:42 -0500

    easy run code

--- runCode.py
+++ runCode.py
@@ -23,7 +23,7 @@ imagein=["","IMG_170805_165709_0138_"]
 image= folderin + imagein[1] + tipo[2] + form[1]
 folder = folderin + "/results/"
 
-skm=segMeanShift.segmentacion(image=image,folder=folder,minc=8,maxc=16,scalefactor=1/2,resize=True)
+skm=segMeanShift.segmentacion(image=image,folder=folder,minc=2,maxc=2,scalefactor=1/2,resize=True)
 
 skm.meanShift()
 
